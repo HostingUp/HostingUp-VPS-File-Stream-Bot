ca5afc06bdd53047092babec05f08a360da07fe5
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2024-09-13 21:03:32 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10.8-slim-buster
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements.txt file into the container at /app
+COPY requirements.txt .
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . .
+
+# Run the application
+CMD ["python", "-m", "biisal"]
---
+++ biisal/vars.py
@@ -0,0 +1,47 @@
+# (c) adarsh-goel (c) @biisal @AmRobots_Bots
+import os
+from os import getenv, environ
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
+bisal_channel = "https://telegram.me/bisal_files"
+bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
+
+class Var(object):
+    MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', '4856555255'))
+    API_HASH = str(getenv('API_HASH', 'gfdbgtgffftgfgf5885454'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '5001854-gyfhhyvgfygbvh'))
+    name = str(getenv('name', 'File_Press_bot'))
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
+    WORKERS = int(getenv('WORKERS', '4'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001626107740'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001626107740'))
+    PORT = int(getenv('PORT', '9097')) #(Here, enter the hosting port you received from HostingUp, which should start with 9000)
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')) #(DO NOT CHANGE IF U DEPLOY YOUR BOT ON HOSTINGUP VPS)
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "4845487454").split()]
+    NO_PORT = bool(getenv('NO_PORT', False))
+    APP_NAME = None
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', '@AmRobots_bots'))
+    if 'DYNO' in environ:
+        ON_HEROKU = True
+        APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
+    
+    else:
+        ON_HEROKU = False
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    HAS_SSL=bool(getenv('HAS_SSL',False))
+    FQDN = 'vps.hostingup.icu:9097' #(Here, enter the domain and hosting port you received from HostingUp. If you're using your own, enter that, but make sure to use the domain name, not the IP address, and include the port as well.)
+    if HAS_SSL:
+        URL = "https://{}/".format(FQDN)
+    else:
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bots_up')) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
+    BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @biisal_bot ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
