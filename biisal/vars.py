9fa8e878352429dcb6904beefc890c0a74747ea1
Anurag Maheshwari <115565191+AM-ROBOTS@users.noreply.github.com>
2024-09-14 15:27:47 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -35,6 +35,7 @@ class Var(object):
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
+    HAS_SSL = False
     FQDN = 'bots.hostingup.icu:9097' #(Here, enter the domain and hosting port you received from HostingUp. If you're using your own, enter that, but make sure to use the domain name, not the IP address, and include the port as well.)
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
